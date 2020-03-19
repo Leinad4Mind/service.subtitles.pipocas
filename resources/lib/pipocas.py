ff70d1ab0e0c60de8fdcf1a787763cd073044f7b
Daniel Mota <daniel@digitalmanager.guru>
2020-03-19 20:33:21 +0000

    Fix code

--- resources/lib/pipocas.py
+++ resources/lib/pipocas.py
@@ -86,18 +86,13 @@ def xbmc_walk(DIR):
 def extract_all_libarchive(archive_file, directory_to, extension):
     overall_success = True
     files_out = list()
-    if 'archive://' in archive_file or 'rar://' in archive_file:
+    if 'rar://' in archive_file:
         archive_path = archive_file
     if extension == "zip":
         archive_path = 'archive://%(archive_file)s' % {'archive_file': urllib.quote_plus(xbmc.translatePath(archive_file))}
     else:
         archive_path = 'rar://%(archive_file)s' % {'archive_file': urllib.quote_plus(xbmc.translatePath(archive_file))}
 
-    if 'archive://' in archive_file or 'rar://' in archive_file:
-        archive_path = archive_file
-    else:
-        archive_path = 'archive://%(archive_file)s' % {
-            'archive_file': urllib.quote_plus(xbmc.translatePath(archive_file))}
     dirs_in_archive, files_in_archive = xbmcvfs.listdir(archive_path)
     for ff in files_in_archive:
         # Windows unexpectedly requires a forward slash in the path
